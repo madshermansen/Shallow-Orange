d76c5316d86cf6037363bd5d2c2f8e99e99bc5e0
Mads Hermansen <mdherman@stud.ntnu.no>
2023-04-04 22:57:14 +0200

    added visualization of movements
    no other changes than implementing chessboard into main file to understand how the engine moves

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from engine import shallowOrange
-from visualizeboard import *
+from chessboard import display
 import time
 
+# initialization variables
+depth = 2
+starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
 if __name__ == "__main__":
-    engine = shallowOrange(depth=4)
+    engine = shallowOrange(starting_fen, depth=depth)
+    
+    # start the display
+    game_board = display.start()
+
     while engine.gameOver() == False:
-        startTime = time.time()
+        update_fen = engine.board.fen()
+        display.update(update_fen, game_board)
         best_move = engine.bestMove()
-        print("Time taken: ", time.time() - startTime)
-        engine.board.push(best_move)
         print(best_move)
+        engine.board.push(best_move)
+        time.sleep(0.5)
 
 
